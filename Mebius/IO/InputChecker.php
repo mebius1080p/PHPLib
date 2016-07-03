9bee52c0ac6a35e85cf14222216a9b012cbdd591
gohei <gohei@dev.com>
2016-07-03 13:53:09 +0900

    Update CheckParamBuilder and InputChecker
    Update CheckParamBuilder and InputChecker for php7

--- Mebius/IO/InputChecker.php
+++ Mebius/IO/InputChecker.php
@@ -1,95 +1,66 @@
 <?php
 namespace Mebius\IO;
 
-//input チェッククラス
+/**
+*InputChecker クラス for php7
+*/
 class InputChecker
 {
 	private $checkArray;//配列
-	private $env;
 	public function __construct(CheckParamBuilder $aCPBObj)
 	{
-		$temp = $aCPBObj->getParam();
-		if(count($temp) === 0)
-		{
+		$paramArray = $aCPBObj->getParam();
+		if (count($paramArray) === 0) {
 			throw new \Exception("CheckParamBuilder オブジェクトの中身が空です");
 		}
-		$this->checkArray = $temp;
-		if ($aCPBObj->getMode() === CheckParamBuilder::GET_POST)
-		{
-			if($_SERVER["REQUEST_METHOD"] === "GET")
-			{
-				$this->env = $_GET;
-			} else {
-				$this->env = $_POST;
-			}
-			$this->check0();
-		} else {//CheckParamBuilder::QUERY_STRING
-			$this->checkQS();
-		}
+		$this->checkArray = $paramArray;
 	}
-	private function check0()
+	/**
+	*バリデートメソッド。複数の値を一気にチェックするので、返り値などは返さない
+	*/
+	public function validate()
 	{
 		$len = count($this->checkArray);
-		for ($i = 0; $i < $len; $i++)
-		{
-			if(array_key_exists($this->checkArray[$i]["name"], $this->env) === false)
-			{
-				throw new \Exception("必須キーが見つかりません");
-			}
-		}
-		$this->check1();
-	}
-	private function check1()
-	{
-		$len = count($this->checkArray);
-		for ($i = 0; $i < $len; $i++)
-		{
-			$name = $this->checkArray[$i]["name"];
+		for ($i = 0; $i < $len; $i++) {
+			$value = $this->checkArray[$i]["value"];
 			$reg = $this->checkArray[$i]["regex"];
-			$ieFlag = $this->checkArray[$i]["ieFlag"];
-			if (mb_check_encoding($this->env[$name], 'UTF-8') === false)
-			{//攻撃の可能性
-				throw new \Exception($name." : パラメーターが UTF-8 ではありません");
+			$isInclude = $this->checkArray[$i]["isInclude"];
+			if (!mb_check_encoding($value, 'UTF-8')) {//攻撃の可能性
+				throw new \Exception($value . " : パラメーターが UTF-8 ではありません");
 			}
-			if ($reg !== "")
-			{
-				if ($ieFlag === CheckParamBuilder::INCLUDE_MATCH)
-				{
-					if(preg_match($reg, $this->env[$name]) !== 1)
-					{
-						throw new \Exception($name." : 値が不正です");
+			if ($reg !== "") {
+				if ($isInclude) {
+					if(preg_match($reg, $value) !== 1) {
+						throw new \Exception($value . " : 値が不正です");
 					}
 				} else {//除外キーワードがないか調べる
-					$result = preg_match($reg, $this->env[$name]);
-					if ($result === 1 || $result === false)
-					{
-						throw new \Exception($name." : 不正な値が含まれています");
+					$result = preg_match($reg, $value);
+					if ($result === 1 || $result === false) {
+						throw new \Exception($value . " : 不正な値が含まれています");
 					}
 				}
 			}//空文字の時はチェックしない
 		}
 	}
-	private function checkQS()
+	/**
+	*メールアドレスだけをチェックするスタティックメソッド
+	*@param {string} $aMail メールアドレスと思われる文字列
+	*@return {stdClass} バリデーション通過したかどうかのフラグとメッセージを持つオブジェクト
+	*/
+	public static function checkMailAddress(string $aMail)
 	{
-		$reg = $this->checkArray[0]["regex"];
-		if (preg_match($reg, $_SERVER['QUERY_STRING']) !== 1)
-		{
-			throw new \Exception("Query_String 値が不正です");
+		$retObj = new \stdClass();
+		$retObj->result = true;
+		$retObj->message = "";
+		//no need to check control string???
+		// if (preg_match('/\A[[:^cntrl:]]{0,50}\z/u', $aMail) !== 1) {
+		// 	$retObj->result = false;
+		// 	$retObj->message = "including controll string";
+		// }
+		if (!filter_var($aMail, FILTER_VALIDATE_EMAIL)) {
+			$retObj->result = false;
+			$retObj->message = "不正なメールアドレスです。";
 		}
-	}
-	public static function checkMailAddress($aMail)
-	{
-		if (is_string($aMail) === false)
-		{
-			throw new \Exception("引数は文字列です");
-		}
-		if (preg_match('/\A[[:^cntrl:]]{0,50}\z/u', $aMail) !== 1)
-		{
-			throw new \Exception("including controll string");
-		}
-		if (filter_var($aMail, FILTER_VALIDATE_EMAIL) === false)
-		{
-			throw new \Exception("不正なメールアドレスです。");
-		}
+		return $retObj;
 	}
 }
