288d58cf89ea2cc1d5375f9e599a70bc0aab8343
mebius <mebius1080p@gmail.com>
2017-12-03 23:30:27 +0900

    added conditionbuilder

---
+++ Mebius/DB/ConditionBuilder.php
@@ -0,0 +1,42 @@
+<?php
+namespace Mebius\DB;
+
+/**
+ * ConditionBuilder 投稿データから sql の where 文とプレースホルダー用データ配列を作成する抽象クラス
+ */
+abstract class ConditionBuilder
+{
+	protected $inputObj = null;
+	protected $condition = "";
+	protected $placeholder = [];
+	/**
+	 * コンストラクタ
+	 * @param mixed $inputObj stdClass や入力データをまとめたクラスのインスタンスなど
+	 */
+	public function __construct($inputObj)
+	{
+		$this->inputObj = $inputObj;
+		$this->build();
+	}
+	/**
+	 * where 文を作成し、placeholder に値を格納するこのクラスの核となるメソッド。
+	 * 抽象クラスで中身を実装すること
+	 */
+	abstract public function build();
+	/**
+	 * 検索条件など、where 文を取得するメソッド
+	 * @param string where 文
+	 */
+	public function getCondition(): string
+	{
+		return $this->condition;
+	}
+	/**
+	 * プレースホルダーに使う配列を取得するメソッド
+	 * @return array プレースホルダーに使う配列
+	 */
+	public function getPlaceHolder(): array
+	{
+		return $this->placeholder;
+	}
+}
