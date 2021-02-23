386e7b63082bae98feab6123d3924f0d26245435
mebius <mebius1080p@gmail.com>
2021-02-23 18:47:57 +0900

    simplecommit で、リフレクションを使ってより簡単にコミットできるようにした

--- Mebius/DB/TCommit.php
+++ Mebius/DB/TCommit.php
@@ -17,13 +17,22 @@ trait TCommit
 	/**
 	 * 単一のレコードをトランザクションを使って書き込むメソッド
 	 * @param string $table テーブル名
-	 * @param string[] $columns テーブルのカラム $placeHolder と同じ数にすること
-	 * @param array $placeHolder 単純配列。$columns と同じ数にすること
+	 * @param object $obj 挿入するデータを収めたオブジェクト。プロパティはテーブルのカラムと 1:1 であること
 	 * @return int insertid 使うかどうかは使用元で決めること
 	 * @throws \Exception エラーで例外
 	 */
-	public function simpleCommit(string $table, array $columns, array $placeHolder): int
+	public function simpleCommit(string $table, object $obj): int
 	{
+		$columns = [];
+		$placeHolder = [];
+		$ref = new \ReflectionClass($obj);
+		$properties = $ref->getProperties();
+		foreach ($properties as $prop) {
+			$propName = $prop->getName();
+			$columns[] = $propName;
+			$placeHolder[] = $obj->$propName;
+		}
+
 		$ih = new InsertHelper2($table, $columns);
 		$sql = $ih->getOnDuplicateSQL();
 		$this->begin();
