9a046d8a1c6ebdb17f65362af38cc6a2000e61fe
mebius <mebius1080p@gmail.com>
2022-03-02 23:05:52 +0900

    pagingcalculator2 をリファクタしバグも潰した

--- Mebius/Paging/PagingCalculator2.php
+++ Mebius/Paging/PagingCalculator2.php
@@ -10,23 +10,39 @@ namespace Mebius\Paging;
 class PagingCalculator2
 {
 	/**
-	 * @var int 一回の検索で取り出す最大件数
+	 * @var int $perPage 一回の検索で取り出す最大件数
 	 */
 	private int $perPage = 1;
 	/**
-	 * @var int 取り出すページ数。取り出すページが異常な値であってもそれを使用する
+	 * @var int $page 取り出すページ数。取り出すページが異常な値であってもそれを使用する
 	 */
 	private int $page = 1;
+	/**
+	 * @var int $count 検索結果のレコード数
+	 */
+	private int $count = 0;
+	/**
+	 * @var int $totalPage 検索結果を分割した場合の全ページ数
+	 */
+	private int $totalPage = 0;
 
 	/**
 	 * コンストラクタ
 	 * @param int $perPage 一回の検索で表示するレコードの数 1 以上
+	 * @param int $recordCount 検索結果数 0以上
 	 * @param int $postedPage 投稿されてきた、取得する希望のページ数 <= 1
 	 */
-	public function __construct(int $perPage, int $postedPage)
+	public function __construct(int $perPage, int $recordCount, int $postedPage)
 	{
 		$this->perPage = $perPage <= 0 ? 1 : $perPage;
+		$this->count = $recordCount < 0 ? 0 : $recordCount;
 		$this->page = $postedPage <= 0 ? 1 : $postedPage;
+
+		$this->totalPage = intval(ceil($this->count / $this->perPage));
+		if ($postedPage > $this->totalPage) {
+			//投稿されてきた希望ページ数が実ページ数をオーバーしていた場合は 1 に強制リセット
+			$this->page = 1;
+		}
 	}
 	/**
 	 * sql で使うオフセット値を返す
@@ -38,19 +54,15 @@ class PagingCalculator2
 	}
 	/**
 	 * PagingSearchResult のインスタンスを返すメソッド
-	 * @param int $recordCount 検索結果数
 	 * @return PagingSearchResult PagingSearchResult のインスタンス
 	 */
-	public function getPagingSearchResult(int $recordCount): PagingSearchResult
+	public function getPagingSearchResult(): PagingSearchResult
 	{
-		$count = $recordCount < 0 ? 0 : $recordCount;
-		$totalPage = ceil($count / $this->perPage);
-
 		$psr = new PagingSearchResult();
-		$psr->total = $count;
+		$psr->total = $this->count;
 		$psr->page = $this->page;
 		$psr->perpage = $this->perPage;
-		$psr->totalpage = (int)$totalPage;
+		$psr->totalpage = $this->totalPage;
 		return $psr;
 	}
 }
--- test/PagingCaluculator2Test.php
+++ test/PagingCaluculator2Test.php
@@ -5,26 +5,49 @@ use Mebius\Paging\PagingCalculator2;
 
 class PagingCalculator2Test extends TestCase
 {
-	public function testNormal()
+	public function testGetOffset()
 	{
-		$pc = new PagingCalculator2(5, 1);
+		$pc = new PagingCalculator2(5, 10, 1);
 		$this->assertEquals(0, $pc->getOffset());
 
-		$pc = new PagingCalculator2(5, 2);
+		$pc = new PagingCalculator2(5, 10, 2);
 		$this->assertEquals(5, $pc->getOffset());
 
-		$pc = new PagingCalculator2(-5, -2);
+		$pc = new PagingCalculator2(-5, 10, -2);
 		$this->assertEquals(0, $pc->getOffset());
 	}
-	public function testSearchResult()
+	public function testSearchResultNormal()
 	{
-		$pc = new PagingCalculator2(5, 1);
+		$pc = new PagingCalculator2(5, 4, 1);
 		$this->assertEquals(0, $pc->getOffset());
 
-		$psr = $pc->getPagingSearchResult(10);
-		$this->assertEquals(2, $psr->totalpage);
+		$psr = $pc->getPagingSearchResult();
+		$this->assertEquals(4, $psr->total);
+		$this->assertEquals(1, $psr->page);
+		$this->assertEquals(5, $psr->perpage);
+		$this->assertEquals(1, $psr->totalpage);
+	}
+	public function testSearchResultNormalB()
+	{
+		$pc = new PagingCalculator2(5, 8, 2);
+		$this->assertEquals(5, $pc->getOffset());
 
-		$psr = $pc->getPagingSearchResult(-1);
-		$this->assertEquals(0, $psr->totalpage);
+		$psr = $pc->getPagingSearchResult();
+		$this->assertEquals(8, $psr->total);
+		$this->assertEquals(2, $psr->page);
+		$this->assertEquals(5, $psr->perpage);
+		$this->assertEquals(2, $psr->totalpage);
+	}
+	public function testSearchResult要求ページ数超過()
+	{
+		//2 ページ分しか取得できないところ、6 ページ目を希望した場合
+		$pc = new PagingCalculator2(5, 10, 6);
+		$this->assertEquals(0, $pc->getOffset());
+
+		$psr = $pc->getPagingSearchResult();
+		$this->assertEquals(10, $psr->total);
+		$this->assertEquals(1, $psr->page);//1 にリセットされる
+		$this->assertEquals(5, $psr->perpage);
+		$this->assertEquals(2, $psr->totalpage);
 	}
 }
